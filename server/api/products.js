43842788d0645579544ee58d885f89c0414bcdf1
millyrocc <mili.conde24@yahoo.com>
2021-12-11 04:15:38 -0500

    completed backend routes for Admin user

--- server/api/products.js
+++ server/api/products.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const {
   models: { Product },
 } = require('../db');
+const User = require('../db/models/User');
 module.exports = router;
 //OUTSTANDING:
 // - Need to create "Cart" & "Order" models in the db folder
@@ -35,6 +36,30 @@ router.get('/:productId', async (req, res, next) => {
 router.post('/', async (req, res, next) => {
   try {
     res.status(201).send(await Product.create(req.body));
+  } catch (error) {
+    next(error);
+  }
+});
+
+//PUT request (an admin updating a product)
+// how to figure out thr isAdmin (demo isAdmin next to include)
+router.put('/:productId', async (req, res, next) => {
+  try {
+    const product = await Product.findByPk(req.params.productId, {
+      include: User,
+    });
+    await product.update();
+    res.send(product);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete('/:productId', async (req, res, next) => {
+  try {
+    const product = await Product.findByPk(req.params.productId, {});
+    await product.destroy();
+    res.send(product);
   } catch (error) {
     next(error);
   }
